eb620383a6f4f827a6e93890add100bcee523c33
Florencia Accastelli <f.accastelli@gmail.com>
2024-07-24 11:33:09 -0300

    add dark mode

---
+++ src/components/DarkModeSwitch.jsx
@@ -0,0 +1,26 @@
+'use client'
+
+import { MdLightMode, MdDarkMode } from 'react-icons/md';
+import { useTheme } from 'next-themes';
+
+import React, { useEffect, useState } from 'react'
+
+export default function DarkModeSwitch() {
+    const {theme, setTheme, systemTheme} = useTheme();
+    const [mounted, setMounted] = useState(false);
+    const currentTheme = theme === 'system' ? systemTheme : theme;
+    useEffect(() => {
+        setMounted(true)
+    }, [])
+  return (
+    <div>
+        {
+            mounted && (currentTheme === 'dark' 
+            ? 
+            (<MdDarkMode onClick={() => setTheme('light')} className='text-xl cursor-pointer hover:text-amber-500'/>) 
+            : 
+            (<MdLightMode onClick={() => setTheme('dark')} className='text-xl cursor-pointer hover:text-amber-500'/>)
+        )}
+    </div>
+  )
+}
